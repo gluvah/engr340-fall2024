4b8515ef993625f4ee1569f2499c5ac9dc97d31a
gluvah <159924221+gluvah@users.noreply.github.com>
2024-10-30 17:13:17 -0400

    Update ekg-signal-processing-template.py

--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,33 +18,49 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
-## YOUR CODE HERE ##
+signal = np.loadtxt(signal_filepath, delimiter=',', skiprows=2)
+first_lead = signal[:3600,1]
+second_lead = signal[:3600,2]
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
 """
 
-## YOUR CODE HERE ##
-
 """
 Step 3: Pass data through weighted differentiator
 """
 
-## YOUR CODE HERE ##
-
-
+# Weighted differentiator
+weighted_data_first = np.diff(first_lead)
+weighted_data_second = np.diff(second_lead)
 """
 Step 4: Square the results of the previous step
 """
- ## YOUR CODE HERE ##
+
+squared_first = np.square(weighted_data_first)
+squared_second = np.square(weighted_data_second)
 
 """
 Step 5: Pass a moving filter over your data
 """
 
-## YOUR CODE HERE
+# Moving average filter
+window_size = 10
+moving_first = np.convolve(squared_first, np.ones(window_size) / window_size, mode='valid')
+moving_second = np.convolve(squared_second, np.ones(window_size) / window_size, mode='valid')
+
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+plt.title('Signal Processing')
+#plt.plot(first_lead, label='Original Signal for Lead 1')
+#plt.plot(second_lead, label='Original Signal for Lead 2')
+#plt.plot(weighted_data_first, label='Lead 1: Weighted')
+#plt.plot(weighted_data_second, label='Lead 2: Weighted')
+#plt.plot(squared_first, label='Lead 1: Squared')
+#plt.plot(squared_second, label='Lead 2: Squared')
+plt.plot(moving_first, label='Lead 1: Moving')
+plt.plot(moving_second, label='Lead 2: Moving')
+plt.xlabel('Sample #')
+plt.ylabel('Voltage (mV)')
+plt.legend()
 plt.show()
+
